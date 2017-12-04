ca1126b7be3a5fb2295851d849298cba6f500cb0
soqutto <me@sokutto.com>
2017-12-05 00:37:41 +0900

    Added terminal color inspection tool in tools/ directory.

---
+++ tools/color.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+
+# Reference: @hidai@github
+# https://qiita.com/hidai@github/items/1704bf2926ab8b157a4f
+
+print "  --   "
+for b in 40..47
+  s = b.to_s
+  print "\033[", s, "m    ", s, "   \033[0m "
+end
+print "\n"
+for c in [ 30, 31, 32, 33, 34, 35, 36, 37, 90, 91, 92, 93, 94, 95, 96, 97 ]
+  s = c.to_s
+  print "\033[", s, "m ", s, "   \033[0m "
+  for b in 40..47
+    s = c.to_s + ";" + b.to_s
+    print "\033[", s, "m ", s, "   \033[0m "
+  end
+  print "\n"
+  for a in [ 1, 4 ]
+    s = c.to_s + ";" + a.to_s
+    print "\033[", s, "m ", s, " \033[0m "
+    for b in 40..47
+      s = c.to_s + ";" + b.to_s + ";" + a.to_s
+      print "\033[", s, "m ", s, " \033[0m "
+    end
+    print "\n"
+  end
+end
